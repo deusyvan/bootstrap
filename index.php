e9bded10ccb846bd62985645f117c6f5175579de
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-06-04 14:44:50 -0300

    Estrutura para bootstrap

---
+++ index.php
@@ -0,0 +1,12 @@
+<!Doctype html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Bootstrap</title>
+		
+	</head>
+	<body>
+		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	</body>
+</html>
