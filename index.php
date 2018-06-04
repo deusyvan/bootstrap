f32409bf5e02859e6e2e6f30910678b2f327e704
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-06-04 14:50:46 -0300

    Importações pronta para uso

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>Bootstrap</title>
-		
-	</head>
-	<body>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="css/bootstrap.min.css" />
 		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
 		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	</head>
+	<body>
 	</body>
 </html>
