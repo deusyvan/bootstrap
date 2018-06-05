923bb85c71b600324063a0cb9628d552afd77cf4
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-06-05 10:50:55 -0300

    Informando que este botão vai ser dropdown

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 			
 			<hr/>
 			<div class="dropdown">
-				<button class="btn btn-primary">Escolha</button>
+				<button class="btn btn-primary dropdown-toggle" data-toggle="dropdown" >Escolha</button>
 			</div>
 			
 			
