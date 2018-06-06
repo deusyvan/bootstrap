5ed6e10d755ef511d2df2691627ad277a5de29c5
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-06-06 15:35:02 -0300

    Deixando um item ativo

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 			<div class="list-group">
 				<a href="" class="list-group-item">Item 1</a>
 				<a href="" class="list-group-item disabled">Item 2</a>
-				<a href="" class="list-group-item">Item 3</a>
+				<a href="" class="list-group-item active">Item 3</a>
 				<a href="" class="list-group-item">Item 4</a>
 				<a href="" class="list-group-item">Item 5</a>
 			</div>
