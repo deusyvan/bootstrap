f9675de9f651d86f345f8e491431c12c2dcfca3b
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-06-06 15:47:45 -0300

    Trabalhando com tab, usando um ul colocamos a classe nav pra dizer que é de navegação e um nav-tabs para ser uma aba

--- index.php
+++ index.php
@@ -15,16 +15,9 @@
 			
 			<hr/>
 			
-			
-			<div class="list-group">
-				<a href="" class="list-group-item list-group-item-success">Item 1</a>
-				<a href="" class="list-group-item disabled">Item 2</a>
-				<a href="" class="list-group-item active">Item 3</a>
-				<a href="" class="list-group-item list-group-item-info">Item 4</a>
-				<a href="" class="list-group-item list-group-item-warning">Item 5</a>
-				<a href="" class="list-group-item list-group-item-danger">Item 6</a>
-			</div>
-			
+			<ul class="nav nav-tabs">
+				
+			</ul>
 			
 		</div>
 	</body>
