bcc56dfdf19b9dcf119244f8658afcfbd3cd0883
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-06-07 15:40:41 -0300

    Criando o conteúdo interno da janela: 1 - criamos o botão de fechar com sua ação com um x de fechar

--- index.php
+++ index.php
@@ -20,7 +20,9 @@
 			<div class="modal" role="dialog" id="janela">
 				<div class="modal-dialog">
 					<div class="modal-content">
-					
+						<div class="modal-header">
+							<button class="close" data-dismiss="modal">&times;</button>
+						</div>
 					</div>
 				</div>
 			</div>
