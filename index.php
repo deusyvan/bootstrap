0d60443a0e5255450212373653fc5071650dd740
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-06-07 10:20:46 -0300

    Ativando uma página e desabilitando outra

--- index.php
+++ index.php
@@ -16,11 +16,11 @@
 			<hr/>
 			
 			<ul class="pagination">
-				<li><a href="">1</a></li>
+				<li class="active"><a href="">1</a></li>
 				<li><a href="">2</a></li>
 				<li><a href="">3</a></li>
 				<li><a href="">4</a></li>
-				<li><a href="">5</a></li>
+				<li class="disabled"><a href="">5</a></li>
 				<li><a href="">6</a></li>
 				
 			</ul>
